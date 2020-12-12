4195277b5898a1a3ec2c64371734f745ade70bc8
Watermelon-c <1973641241@qq.com>
2020-12-12 20:11:02 +0800

    iu

--- js/rem.js
+++
@@ -1,6 +0,0 @@
-	let html = document.querySelector("html");
-		html.style['fontSize'] = html.offsetWidth / 750 * 100 + 'px';
-		addEventListener('resize', () => {
-			html.style['fontSize'] = html.offsetWidth / 750 * 100 + 'px';
-		})
-	console.log('昨日头条v1');
